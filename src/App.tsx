a48b9d42c6050eda5eb16a5818627a5d0d7734c8
anwrat <anweshrwt22@gmail.com>
2026-04-16 17:05:02 +0545

    Filtering using selectFromResult inside useRequestQuery hook (does not persist on refresh)

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,21 @@ export default function Search() {
   const [query, setQuery] = useState('')
   const [debouncedQuery, setDebouncedQuery] = useState('')
   const [id, setId] = useState('');
+  const [yearFilter, setYearFilter] = useState('');
+  const [typeFilter, setTypeFilter] = useState('');
 
-  const { data, isLoading } = useSearchMoviesQuery(debouncedQuery, {
+  const { filteredData, isLoading } = useSearchMoviesQuery(debouncedQuery, {
     skip: debouncedQuery.length < 3,
+    selectFromResult: ({data, isLoading}) =>{
+      const filteredData = data?.Search?.filter((movie: any) => {
+        const matchesType = typeFilter ? movie.Type === typeFilter : true;
+        return matchesType;
+      });
+      return {
+        filteredData,
+        isLoading
+      }
+    },
   })
 
   const { data: movieDetails, isLoading: isMovieDetailsLoading } = useGetMovieDetailsQuery(id, {
@@ -32,9 +44,18 @@ export default function Search() {
         onChange={(e) => setQuery(e.target.value)}
       />
 
+      <select onChange={(e)=>setTypeFilter(e.target.value)}>
+        <option value = "">All</option>
+        <option value = "movie">Movies</option>
+        <option value = "series">Series</option>
+        <option value = "episodes">Episodes</option>
+      </select>
+
+
+
       {isLoading && <p>Loading...</p>}
 
-      {query.length >= 3 && data?.Search?.map((movie: any) => (
+      {query.length >= 3 && filteredData?.map((movie: any) => (
         <div key={movie.imdbID}>
           <p onClick = {() => setId(movie.imdbID)}>{movie.Title}</p>
         </div>
