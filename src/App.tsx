a0531c34ac72c0243cf316987af49560fc38afe3
anwrat <anweshrwt22@gmail.com>
2026-04-17 14:01:40 +0545

    Installing react-hook-form, zod, hook resolvers for add movie function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Search from './pages/Search'
 import Favourites from './pages/Favourites'
+import AddMovie from './pages/AddMovie'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Search />} />
         <Route path="/favourites" element={<Favourites />} />
+        <Route path="/add" element={<AddMovie />} />
       </Routes>
     </BrowserRouter>
   )
